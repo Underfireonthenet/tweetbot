b68dffc2b3dea9144a9b9e1a01a1e117e78a13d2
msky026 <pref.myu@gmail.com>
2017-10-14 16:02:39 +0900

    modify time schedule

--- src/tweetbot/tweet.cr
+++ src/tweetbot/tweet.cr
@@ -1,11 +1,20 @@
 require "./twitter_client"
 
-MESSAGES = [
-  "TOKYO MX 「ブレンド・S」 はじまるよー #ブレンドS",
-  "ＢＳ１１ 「ブレンド・S」 はじまるよー #ブレンドS",
-]
+MESSAGES = {
+  "tokyomx" => "TOKYO MX 「ブレンド・S」 はじまるよー #ブレンドS",
+  "bs11" => "ＢＳ１１ 「ブレンド・S」 はじまるよー #ブレンドS",
+}
 
 twitter_client = TwitterClient.new
-MESSAGES.each do |message|
-  twitter_client.tweet(message)
+time = Time.new
+
+twitter_client.tweet(MESSAGES["tokyomx"]) if tokyomx?(time)
+twitter_client.tweet(MESSAGES["bs11"]) if bs11?(time)
+
+def tokyomx?(time)
+  time.sunday? && time.hour == 0 && time.minute >=19 && time.minute <= 21
+end
+
+def bs11?(time)
+  time.sunday? && time.hour == 0 && time.minute >=19 && time.minute <= 21
 end
