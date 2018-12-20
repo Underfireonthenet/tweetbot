f8f46487dd92d84a940af26896f534c76d211da0
msky026 <pref.myu@gmail.com>
2018-12-21 00:09:44 +0900

    modify

--- src/tweetbot/models/video.cr
+++ src/tweetbot/models/video.cr
@@ -1,3 +1,10 @@
+require "db"
+require "pg"
+require "granite/adapter/pg"
+
+database_url = ENV["DATABASE_URL"]? || "postgres://preface@localhost:5432/tweetbot_development"
+Granite::Adapters << Granite::Adapter::Pg.new({name: "pg", url: database_url})
+
 class Video < Granite::Base
   adapter pg
   field channel_id : String
--- src/tweetbot/tweet.cr
+++ src/tweetbot/tweet.cr
@@ -1,29 +1,44 @@
 require "google-api-client-cr"
 require "./twitter_client"
-require "db"
-require "pg"
-require "granite/adapter/pg"
-database_url = ENV["DATABASE_URL"]? || "postgres://preface@localhost:5432/tweetbot_development"
-Granite::Adapters << Granite::Adapter::Pg.new({name: "pg", url: database_url})
 require "./models/video"
 
-twitter_client = TwitterClient.new
-youtube = Google::Apis::YoutubeV3::YouTubeService.new
-
 # https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCWzenZSy9GJBcPzdSm-UX5w&order=date
-result = youtube.list_searches("id,snippet", channel_id: "UCWzenZSy9GJBcPzdSm-UX5w", order: "date", max_results: 5)
-exit unless result
-channel_id = "UCWzenZSy9GJBcPzdSm-UX5w"
-video_id = result["items"][0]["id"]["videoId"]
-title = result["items"][0]["snippet"]["title"]
 
-exit unless video_id
+class Notifier
+  @channel_infos : Array(NamedTuple(channel_id: String, tags: String))
+  @twitter_client : TwitterClient
+  @youtube : Google::Apis::YoutubeV3::YouTubeService
 
-video = Video.find_by(video_id: video_id.to_s)
+  def initialize(channel_infos)
+    @channel_infos = channel_infos
+    @twitter_client = TwitterClient.new
+    @youtube = Google::Apis::YoutubeV3::YouTubeService.new
+  end
 
-unless video
-  new_video = Video.new(channel_id: channel_id, video_id: video_id.to_s)
-  new_video.save!
-  message = "#{title} https://www.youtube.com/watch?v=#{video_id} #モンスト #モンストアニメ"
-  twitter_client.tweet(message)
+  def youtube_channel_info(channel_id)
+    @youtube.list_searches("id,snippet", channel_id: channel_id, order: "date", max_results: 5)
+  end
+
+  def notify_all
+    @channel_infos.each do |channel_info|
+      notify(channel_info[:channel_id], channel_info[:tags])
+    end
+  end
+
+  def notify(channel_id, tags)
+    channel_info = youtube_channel_info(channel_id)
+    return unless channel_info
+    video_id = channel_info["items"][0]["id"]["videoId"]
+    title = channel_info["items"][0]["snippet"]["title"]
+    return unless video_id
+    video = Video.find_by(video_id: video_id.to_s)
+    return if video
+    Video.create!(channel_id: channel_id, video_id: video_id.to_s)
+    message = "#{title} https://www.youtube.com/watch?v=#{video_id.to_s} #{tags}"
+    @twitter_client.tweet(message)
+  end
 end
+
+channel_infos = [{channel_id: "UCWzenZSy9GJBcPzdSm-UX5w", tags: "#モンスト #モンストアニメ"}]
+notifier = Notifier.new(channel_infos)
+notifier.notify_all
